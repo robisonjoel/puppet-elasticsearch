a9e224b61930ec7ff025aa3420f4794912395a37
Nick Chappell <nick@intronic.org>
2013-02-24 02:10:41 -0800

    Created init.pp with a basic outline of what the resources in it will be.

---
+++ manifests/init.pp
@@ -0,0 +1,14 @@
+class elasticsearch {
+
+	#file
+		#/etc/elasticsearch/elasticsearch.yml
+		#elasticsearch .deb pacakge: 
+	#packages
+		#JRE
+		#elasticsearch package itself
+			#might be able to work as an HTTP source: http://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.20.5.deb
+	#service
+		#elasticsearch service
+	
+
+}
