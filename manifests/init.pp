8193f603b463997e847c0b9965243fee41172ca6
Nick Chappell <nickc@puppetlabs.com>
2013-02-24 20:53:12 -0800

    Rearranged pseudocode.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,14 +1,15 @@
 class elasticsearch {
 
-	#file
-		#/etc/elasticsearch/elasticsearch.yml
-		#elasticsearch .deb pacakge: 
-	#packages
-		#JRE
-		#elasticsearch package itself
-			#might be able to work as an HTTP source: http://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.20.5.deb
-	#service
-		#elasticsearch service
-	
-
+    #file
+    	#/etc/elasticsearch/elasticsearch.yml
+        #elasticsearch .deb pacakge: 
+    #packages
+        
+    
+        #elasticsearch package itself
+            #might be able to work as an HTTP source: http://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.20.5.deb
+    #service
+        #elasticsearch service
+    
+            
 }
