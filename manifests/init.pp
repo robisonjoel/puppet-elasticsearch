df4d69358a64b79bd55a19a71ddbe6ed199175af
Nick Chappell <nickc@puppetlabs.com>
2013-02-26 22:29:31 -0800

    Parameterized the class and added in-manifest documentation.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,54 +1,72 @@
-class elasticsearch {
-	
-	#Variables (will make these parameters later so the class can be parameterized
-	#The cluster name
-	$cluster_name = 'mycluster01'
-	#The network interface on which nodes will communicate with each other
-	$bind_interface = 'eth1'
-	
-	
-	#File resources
-	
-	#Elasticsearch's main config file and the directory its found in
-	file { '/etc/elasticsearch/':
-	  ensure => directory,
-	}
-		
-	file { 'elasticsearch.yml':
-	  path => '/etc/elasticsearch/elasticsearch.yml',
-	  ensure => file,
-	  content => template('elasticsearch/elasticsearch.yml.erb'),
-	  require => File['/etc/elasticsearch/'],
-	}
-	
-	#ElasticSearch .DEB package; this isn't in most repos, so we're copying it down first
-	#here and installing it with the package resource farther down below.
-	file { 'elasticsearch-package':
-	  path => '/tmp/elasticsearch.deb',
-	  ensure => present,
-	  source => 'puppet:///modules/elasticsearch/elasticsearch.deb',
-	}
+# == Class: elasticsearch
+#
+# This base class installs and configures an ElasticSearch node with basic settings.
+# Multicast networking is used (it's ElasticSearch's default).
+#
+# === Parameters
+# [*cluster_name*]
+#    The name of the ElasticSearch cluster that this node will be a part of. This is used
+#    when automatic multicast node discovery is used, as is the case with ElasticSearch.
+#    Defaults to "mycluster01".    
+#
+# [*bind_interface*]
+#    The network interface on which discovery and inter-node communication will be done.
+#
+# === Examples
+# 
+#   class {
+#     cluster_name     => 'logstash-cluster',
+#     bind_interface   => 'eth1',
+#   }
+#
+#
+class elasticsearch (
+  $cluster_name = 'mycluster01',
+  $bind_interface = 'eth1'
+) {	
     
+    #File resources
+    
+    #Elasticsearch's main config file and the directory its found in
+    file { '/etc/elasticsearch/':
+      ensure => directory,
+    }
+    
+    file { 'elasticsearch.yml':
+      path => '/etc/elasticsearch/elasticsearch.yml',
+      ensure => file,
+      content => template('elasticsearch/elasticsearch.yml.erb'),
+      require => File['/etc/elasticsearch/'],
+    }
+
+    #ElasticSearch .DEB package; this isn't in most repos, so we're copying it down first
+    #here and installing it with the package resource farther down below.
+    file { 'elasticsearch-package':
+      path => '/tmp/elasticsearch.deb',
+      ensure => present,
+      source => 'puppet:///modules/elasticsearch/elasticsearch.deb',
+    }
+
     #Package resources
-    
+
     #The OpenJDK Java 7 runtime; ElasticSearch is written in Java and requires this to run
-	package { 'openjdk-7-jre-headless':
-	  ensure => installed,
-	  provider => 'apt',
-	}
-    
+    package { 'openjdk-7-jre-headless':
+      ensure => installed,
+      provider => 'apt',
+    }
+
     #Elasticsearch itself
     #The source is the 'elastic-search' package farther above
-	package { 'elasticsearch':
-	  source => '/tmp/elasticsearch.deb',
-	  name => 'elasticsearch-0.20.5.deb',
-	  ensure => latest,
-	  provider => dpkg,
-	  require => [Package['openjdk-7-jre-headless'], File['elasticsearch-package']],
-	}
-    
+    package { 'elasticsearch':
+      source => '/tmp/elasticsearch.deb',
+      name => 'elasticsearch-0.20.5.deb',
+      ensure => installed,
+      provider => dpkg,
+      require => [Package['openjdk-7-jre-headless'], File['elasticsearch-package']],
+    }
+
     #Service resources
-    
+
     #ElasticSearch service
     service { 'elasticsearch':
       enable => true,
@@ -56,5 +74,5 @@ class elasticsearch {
       require => Package['elasticsearch', 'openjdk-7-jre-headless'],
       subscribe => File['elasticsearch.yml'],
     }
-            
+        
 }
